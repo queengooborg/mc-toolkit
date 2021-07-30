269bb6ed047cd6bc82a2ca1c3d9fedc342cb92c6
Queen Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2021-07-29 21:54:41 -0700

    Update compatible version list

--- parse_items.py
+++ parse_items.py
@@ -6,7 +6,7 @@
 # This code is licensed under the GNU GPLv3 license (https://choosealicense.com/licenses/gpl-3.0/).
 #
 # This program takes a decompiled version of Minecraft and create an ordered list of all the blocks and their designated tabs.  After that, by taking an EssentialsX worth.yml, generates an item shop for BossShopPro + BS-ItemShops on Bukkit/Spigot/Paperclip servers.
-# Developed and tested for 1.14 through 1.16, may work on all versions from 1.13 on.
+# Developed and tested for 1.14 through 1.17.
 #
 # Requirements:
 # - Python 3.9 (earlier Python 3 versions may work but not recommended)
