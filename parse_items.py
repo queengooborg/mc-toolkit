cb19a68cc2d232f7276c07b81b2472a4125f8d79
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2023-09-01 02:24:47 -0700

    Update and overhaul for 1.20

--- parse_items.py
+++ parse_items.py
@@ -6,7 +6,7 @@
 # This code is licensed under the GNU GPLv3 license (https://choosealicense.com/licenses/gpl-3.0/).
 #
 # This program takes a decompiled version of Minecraft and create an ordered list of all the blocks and their designated tabs.  After that, by taking an EssentialsX worth.yml, generates an item shop for BossShopPro + BS-ItemShops on Bukkit/Spigot/Paperclip servers.
-# Developed and tested for 1.14 through 1.19.
+# Developed and tested for 1.14 through 1.20. (Note: 1.20 implementation is currently experimental)
 #
 # Requirements:
 # - Java 8+
@@ -31,18 +31,6 @@ script_dir = os.path.dirname(__file__)
 outpath = os.path.join(script_dir, "output")
 
 worth = yaml.load(open(os.path.join(script_dir, "worth.yml"), 'r'), Loader=Loader).get('worth', {})
-
-shop_blocks = {
-	'BUILDING_BLOCKS': 'BRICKS',
-	'DECORATIONS': 'PEONY',
-	'REDSTONE': 'REDSTONE',
-	'TRANSPORTATION': 'POWERED_RAIL',
-	'MISC': 'LAVA_BUCKET',
-	'FOOD': 'APPLE',
-	'TOOLS': 'IRON_AXE',
-	'COMBAT': 'GOLDEN_SWORD',
-	'BREWING': 'POTION',
-}
 
 # Items that will not yield a missing item warning
 ignored_items = "(" + ")|(".join([
@@ -88,17 +76,58 @@ def prepare_source(mc_version):
 	source_path = glob.glob(str(sourcepath))
 	return Path(f"{source_path[0]}/net/minecraft")
 
-def get_items(source_path, mc_version):
-	if mc_version < '1.17':
-		itemgroupname = 'ItemGroup'
-		itemgroupjava = Path(f"{source_path}/item/{itemgroupname}.java")
-		itemsjava = Path(f"{source_path}/item/Items.java")
-	else:
+# Get creative mode items list for 1.20+
+# Experimental, may be buggy
+def get_items_120(source_path, mc_version):
+	items = OrderedDict()
+	groups = {}
+
+	creativemodetabjava = Path(f"{source_path}/world/item/CreativeModeTabs.java")
+
+	with open(str(creativemodetabjava)) as cmtj:
+		current_group = None
+
+		# Reading source code line by line to avoid regex backtracking issues
+		for line in cmtj.readlines():
+			groupmatch = re.search(r"Registry\.register\(registry, ([\w_]+), CreativeModeTab\.builder\(CreativeModeTab\.Row\.(?:TOP|BOTTOM), \d+\)\.title\(Component.translatable\(\"itemGroup\.\w+\"\)\)\.icon\(\(\) -> new ItemStack\((?:Items|Blocks)\.(\w+)\)\)\.displayItems\(\(itemDisplayParameters, output\) -> {", line)
+			if groupmatch and groupmatch.group(1) not in ['SPAWN_EGGS', 'OP_BLOCKS']:
+				current_group = groupmatch
+
+			itemmatch = re.search(r"output\.accept\(Items\.([\w_]+)\);", line)
+			if itemmatch:
+				if current_group.group(1) in items:
+					items[current_group.group(1)].append(itemmatch.group(1).lower())
+				else:
+					groups[current_group.group(1)] = current_group.group(2)
+					items[current_group.group(1)] = [itemmatch.group(1).lower()]
+
+	return (groups, items)
+
+def get_items_113(source_path, mc_version):
+	items = OrderedDict()
+
+	groups = {
+		'BUILDING_BLOCKS': 'BRICKS',
+		'DECORATIONS': 'PEONY',
+		'REDSTONE': 'REDSTONE',
+		'TRANSPORTATION': 'POWERED_RAIL',
+		'MISC': 'LAVA_BUCKET',
+		'FOOD': 'APPLE',
+		'TOOLS': 'IRON_AXE',
+		'COMBAT': 'GOLDEN_SWORD',
+		'BREWING': 'POTION',
+	}
+
+	if mc_version >= '1.17':
+		# 1.17-1.19
 		itemgroupname = 'CreativeModeTab'
 		itemgroupjava = Path(f"{source_path}/world/item/{itemgroupname}.java")
 		itemsjava = Path(f"{source_path}/world/item/Items.java")
-
-	items = OrderedDict()
+	else:
+		# 1.13-1.16
+		itemgroupname = 'ItemGroup'
+		itemgroupjava = Path(f"{source_path}/item/{itemgroupname}.java")
+		itemsjava = Path(f"{source_path}/item/Items.java")
 
 	with open(str(itemgroupjava), 'r') as igj:
 		for line in igj.readlines():
@@ -121,7 +150,12 @@ def get_items(source_path, mc_version):
 				if match2:
 					items['MISC'].append(match2.group(1).lower())
 
-	return items
+	return (groups, items)
+
+def get_items(source_path, mc_version):
+	if mc_version >= '1.20':
+		return get_items_120(source_path, mc_version)
+	return get_items_113(source_path, mc_version)
 
 def make_shops(mc_version):
 	if not mc_version:
@@ -131,7 +165,7 @@ def make_shops(mc_version):
 	if not source_path:
 		return False
 
-	all_items = get_items(source_path, mc_version)
+	(shop_blocks, all_items) = get_items(source_path, mc_version)
 
 	os.makedirs(outpath, exist_ok=True)
 
