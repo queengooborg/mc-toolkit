9c3724081a107ac550540ccde0e45bb910efa632
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2023-10-17 08:54:09 -0700

    Fix import issue

--- lib/__init__.py
+++ lib/__init__.py
@@ -6,7 +6,7 @@
 # This code is licensed under the GNU GPLv3 license (https://choosealicense.com/licenses/gpl-3.0/).
 #
 
-from .get_items import main as get_items, get_items_list
+from .get_items import main as get_items
 from .run_subprocess import main as run_subprocess
 from .prepare_source import main as prepare_source
 from .creative_only_items import creative_only_items
