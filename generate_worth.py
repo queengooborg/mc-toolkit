66b25006de56d3185deb1b29f1041599dc15be95
Queen Vinyl Da.i'gyu-Kazotetsu <vinyldarkscratch@gmail.com>
2024-07-13 13:28:46 -0700

    Utilize vanilla Minecraft names first, then remap all at once
    This is to add compatibility with non-Essentials plugins that may use the same worth.yml file, but with vanilla Minecraft item names

--- generate_worth.py
+++ generate_worth.py
@@ -28,17 +28,17 @@ worth_header = open(script_dir / "worth_yml_header.yml", "r").read()
 
 def remap_ingredient(item_id):
 	remappings = {
-		'PLANKS': 'oakplanks',
-		'LOGS': 'oaklog',
-		'LOGS_THAT_BURN': 'oaklog',
-		'WOODEN_SLABS': 'oakslab',
-		'COAL/CHARCOAL': 'coal',
-		'COALS': 'coal',
-		'WOOL': 'whitewool',
-		'SOUL_FIRE_BASE_BLOCKS': 'soulsoil',
-		'STONE_CRAFTING_MATERIALS': 'cobblestone',
-		'STONE_TOOL_MATERIALS': 'cobblestone',
-		'SAND/RED_SAND': 'sand',
+		'PLANKS': 'OAK_PLANKS',
+		'LOGS': 'OAK_LOG',
+		'LOGS_THAT_BURN': 'OAK_LOG',
+		'WOODEN_SLABS': 'OAK_SLAB',
+		'COAL/CHARCOAL': 'COAL',
+		'COALS': 'COAL',
+		'WOOL': 'WHITE_WOOL',
+		'SOUL_FIRE_BASE_BLOCKS': 'SOUL_SOIL',
+		'STONE_CRAFTING_MATERIALS': 'COBBLESTONE',
+		'STONE_TOOL_MATERIALS': 'COBBLESTONE',
+		'SAND/RED_SAND': 'SAND',
 	}
 
 	if item_id in remappings:
@@ -50,7 +50,7 @@ def remap_ingredient(item_id):
 	elif item_id.endswith('_BLOCKS'):
 		item_id = item_id.replace('_BLOCKS', '_BLOCK')
 
-	return item_id.replace('_', '').lower()
+	return item_id
 
 def add_to_worth(worth, item_id, value):
 	worth[item_id] = round(value, 2)
@@ -87,35 +87,34 @@ def calculate_worth_from_recipe(items, worth, item_id):
 
 def calculate_worth(worth, items):
 	for item, recipe in items.items():
-		item_id = item.replace('_', '').lower()
 
-		if item_id in worth:
+		if item in worth:
 			continue # Skip already calculated values
 
 		# Ores
 		if item.endswith('_ORE'):
-			material = item.replace("NETHER_", "").replace("DEEPSLATE_", "").split('_')[0].lower()
-			if material in ['iron', 'copper', 'gold']:
-				material = f'raw{material}'
-			elif material == 'lapis':
-				material = 'lapislazuli'
-			add_to_worth(worth, item_id, worth[material] * 0.75)
+			material = item.replace("NETHER_", "").replace("DEEPSLATE_", "").split('_')[0]
+			if material in ['IRON', 'COPPER', 'GOLD']:
+				material = f'RAW_{material}'
+			elif material == 'LAPIS':
+				material = 'LAPIS_LAZULI'
+			add_to_worth(worth, item, worth[material] * 0.75)
 			continue
 
 		# Oxidized copper blocks (EXPOSED_COPPER, WEATHERED_COPPER, OXIDIZED_COPPER)
 		elif item in ["EXPOSED_COPPER", "WEATHERED_COPPER", "OXIDIZED_COPPER"]:
-			if not 'cutcopper' in worth:
+			if not 'CUT_COPPER' in worth:
 				continue # Need to wait for calculation
-			add_to_worth(worth, item_id, worth['cutcopper'] *
+			add_to_worth(worth, item, worth['CUT_COPPER'] *
 				(0.5 if item == 'EXPOSED_COPPER' else 0.4 if item == 'WEATHERED_COPPER' else 0.3)
 			)
 			continue
 
 		# Damaged anvils (CHIPPED_ANVIL, DAMAGED_ANVIL)
 		elif item.endswith("_ANVIL"):
-			if not 'anvil' in worth:
+			if not 'ANVIL' in worth:
 				continue # Need to wait for calculation
-			add_to_worth(worth, item_id, worth['anvil'] * (0.5 if item == 'CHIPPED_ANVIL' else 0.25))
+			add_to_worth(worth, item, worth['ANVIL'] * (0.5 if item == 'CHIPPED_ANVIL' else 0.25))
 			continue
 
 		if not recipe:
@@ -133,18 +132,23 @@ def calculate_worth(worth, items):
 		if not can_calc:
 			continue
 
-		add_to_worth(worth, item_id, calculate_worth_from_recipe(items, worth, item))
+		add_to_worth(worth, item, calculate_worth_from_recipe(items, worth, item))
 
 		# Handle legacy names
-
 		if item.startswith('END_STONE_BRICK') and item != "END_STONE_BRICKS":
-			worth[item_id.replace('brick', '')] = worth[item_id]
+			worth[item.replace('_BRICK', '')] = worth[item]
 		elif item == "MELON":
-			worth["melonblock"] = worth['melon']
+			worth["MELON_BLOCK"] = worth['MELON']
 		elif item == "SKULL_BANNER_PATTERN":
-			worth["skeletonbannerpattern"] = worth["skullbannerpattern"]
+			worth["SKELETON_BANNER_PATTERN"] = worth["SKULL_BANNER_PATTERN"]
 
-def generate_worth(mc_version, no_cache=False, outpath=output_dir / "worth.yml"):
+def remap_names_for_essentials(worth):
+	new_worth = {}
+	for item, value in worth.items():
+		new_worth[item.replace('_', '').lower()] = value
+	return new_worth
+
+def generate_worth(mc_version, no_cache=False, outpath=output_dir / "worth.yml", essentials=True):
 	if not mc_version:
 		mc_version = get_latest_version()[1]
 
@@ -160,14 +164,16 @@ def generate_worth(mc_version, no_cache=False, outpath=output_dir / "worth.yml")
 		calculated_items = len(worth.keys())
 
 	for i in items:
-		ikey = i.replace("_", "").lower()
-		if ikey not in worth:
+		if i not in worth:
 			print(f'{i} was not calculated!', f'Its recipe was {items[i]}' if items[i] else 'It has no recipe!')
-		elif worth[ikey] == 0.0:
+		elif worth[i] == 0.0:
 			print(f'{i} resulted in a value of 0.00, calculation error!')
 
 
 	os.makedirs(output_dir, exist_ok=True)
+
+	if essentials:
+		worth = remap_names_for_essentials(worth)
 
 	with open(outpath, 'w') as worthfile:
 		worthfile.write(worth_header + "\n\n")
@@ -177,6 +183,7 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(prog="generate_worth", description="Generate an EssentialsX worth.yml file based on Minecraft recipes and a few base prices")
 	parser.add_argument('mc_version', nargs='?')
 	parser.add_argument('-n', '--no_cache', action='store_true')
+	parser.add_argument('-v', '--vanilla', action='store_true')
 	args = parser.parse_args()
 
-	generate_worth(args.mc_version, no_cache=args.no_cache)
+	generate_worth(args.mc_version, no_cache=args.no_cache, essentials=not args.vanilla)
